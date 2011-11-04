7b517a8cc8e024807a48c44e98f94ac4c88c1e48
Tommaso <tommaso.piazza@gmail.com>
2011-11-04 17:43:16 +0100

    Initial Commit

---
+++ Breakout/BRKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BRKAppDelegate.h
+//  Breakout
+//
+//  Created by Tommaso Piazza on 11/4/11.
+//  Copyright (c) 2011 ChalmersTH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BRKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Breakout/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Breakout
+//
+//  Created by Tommaso Piazza on 11/4/11.
+//  Copyright (c) 2011 ChalmersTH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BRKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BRKAppDelegate class]));
+    }
+}
